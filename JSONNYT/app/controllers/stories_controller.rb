0d36eb94309405d2be6158aa48df2791d6a20d23
Patricia Parker <pk.patricia@gmail.com>
2017-07-04 19:10:49 -0500

    Changed Link Format

--- JSONNYT/app/controllers/stories_controller.rb
+++ JSONNYT/app/controllers/stories_controller.rb
@@ -8,7 +8,7 @@ class StoriesController < ApplicationController
   end
 
   def get_stories
-    nyt_URI = "http://api.nytimes.com/svc/mostpopular/v2/mostviewed/politics/1.json?api-key=8015472dd53878d6ea7be25929f90fed%3A5%3A74630877"
+    nyt_URI = "http://api.nytimes.com/svc/mostpopular/v2/mostviewed/all-sections/1.json?api-key=516569f75abbf12cc72f358e303147c9:15:74630877"
     response = Net::HTTP.get_response(URI.parse(nyt_URI))
     nyt_stories_json = JSON.parse(response.body)
     @nyt_stories = nyt_stories_json["results"]
